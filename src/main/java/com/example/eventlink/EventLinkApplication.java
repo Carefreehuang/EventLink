4219c3234b5abdf3cb8453c1a11666f5a9132b91
Carefreehuang <2380520989@qq.com>
2024-12-27 11:36:42 +0800

    init

---
+++ src/main/java/com/example/eventlink/EventLinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventLinkApplication.class, args);
+    }
+
+}
