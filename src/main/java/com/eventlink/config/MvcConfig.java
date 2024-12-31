ff8109eeb53d4491d566441fa769539034bf17a6
Carefreehuang <2380520989@qq.com>
2024-12-31 16:50:40 +0800

    fix:拦截器补充注解

--- src/main/java/com/eventlink/config/MvcConfig.java
+++ src/main/java/com/eventlink/config/MvcConfig.java
@@ -3,10 +3,12 @@ package com.eventlink.config;
 import com.eventlink.utils.interceptor.LoginInterceptor;
 import com.eventlink.utils.interceptor.RefreshTokenInterceptor;
 import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class MvcConfig implements WebMvcConfigurer {
     @Resource
     private StringRedisTemplate stringRedisTemplate;
